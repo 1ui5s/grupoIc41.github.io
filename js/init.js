5a31419fc3955eebce56513053502ee20677ed66
Luis Ledesma <88347870+1ui5s@users.noreply.github.com>
2021-08-02 17:02:36 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,18 +1,18 @@
-/*  Inicializa Firebase con la
+/ * Inicializa Firebase con la
  * configuración del proyecto.
  * Revisa la configuración en tu
  * servidor de Firebase y cópiala
  * aquí sustituyendo los
- * asteriscos. Los campos deben
+* asteriscos. Los campos deben
  * quedar igual que en tu
- * servidor. */
-// @ts-ignore
-firebase.initializeApp({
-    apiKey: "AIzaSyCfMMR4bFjJgfECWOsx6iRCHPZTeDQ9GBQ",
-    authDomain: "bdfalumnos41.firebaseapp.com",
-    projectId: "bdfalumnos41",
-    storageBucket: "bdfalumnos41.appspot.com",
-    messagingSenderId: "463512573259",
-    appId: "1:463512573259:web:2531082c01b911490b04ce",
-    measurementId: "G-DXY3ZYMKY4"
-});
+* servidor. * /
+// @ts -ignore
+base de fuego . initializeApp ( {
+    apiKey: "AIzaSyDmhlaNhML3f_OMQOhLqfYOB8pVerBxVII",
+  authDomain: "tareafinal1.firebaseapp.com",
+  projectId: "tareafinal1",
+  storageBucket: "tareafinal1.appspot.com",
+  messagingSenderId: "753464591206",
+  appId: "1:753464591206:web:009390e317b75d8f78fea7",
+  measurementId: "G-ECPP428JQ9"
+} ) ;
